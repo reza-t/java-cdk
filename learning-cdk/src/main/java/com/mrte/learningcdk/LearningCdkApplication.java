f5c7ac3bf20995d176d55248c1ce814c32773442
Taleghani Esfahani <reza@MN-EX138T.local>
2024-04-15 16:23:21 +0200

    feat: init project

---
+++ learning-cdk/src/main/java/com/mrte/learningcdk/LearningCdkApplication.java
@@ -0,0 +1,13 @@
+package com.mrte.learningcdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningCdkApplication.class, args);
+	}
+
+}
---
+++ learning-cdk/src/test/java/com/mrte/learningcdk/LearningCdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrte.learningcdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
